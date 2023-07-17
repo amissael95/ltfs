74d636e1246986f180c79f02388ccbdf9bb30f69
Missael Palacios <missaelpalacios@ibm.com>
2023-07-17 13:06:50 -0600

    Using print

--- src/main.c
+++ src/main.c
@@ -564,7 +564,7 @@ static int show_device_list(struct ltfs_fuse_data *priv)
 
 int main(int argc, char **argv)
 {
-	ltfsmsg(LTFS_INFO, 14105I, "LTFS_DEBUG: Using LTFS_BSC branch");
+	print("LTFS_DEBUG: Using LTFS_BSC branch");
 	
 	int ret, i, cmd_args_len;
 	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
